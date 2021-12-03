50fd2ef022e3575d6b2d26e3af59dc3d9eb3fb5e
Сергей Фалилеев <falileev.sergey2003@mail.ru>
2021-12-03 10:23:14 +0300

    Добавил код в обработчике события KeyPress для текстового поля и добавил BackgroundWorker на форму.

--- WinBackgroundWorker/Form1.Designer.cs
+++ WinBackgroundWorker/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // label1
@@ -60,6 +61,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(80, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // progressBar1
             // 
@@ -112,6 +114,7 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 

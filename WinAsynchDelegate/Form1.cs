490619d165807289c0185bd88ce5f9e43e03d0e3
Сергей Фалилеев <falileev.sergey2003@mail.ru>
2021-12-03 11:28:39 +0300

    Добавил метод и использовал делагат

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -12,9 +12,16 @@ namespace WinAsynchDelegate
 {
     public partial class Form1 : Form
     {
+        private delegate void TimeConsumingMethodDelegate(int seconds);
         public Form1()
         {
             InitializeComponent();
         }
+        private void TimeConsumingMethod(int seconds)
+        {
+            for(int j=1;j<=seconds;j++)
+                System.Threading.Thread.Sleep(1000);
+        }
+
     }
 }

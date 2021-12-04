e709507d3f8557ca29d504354241828377b3765f
Сергей Фалилеев <falileev.sergey2003@mail.ru>
2021-12-04 10:18:30 +0300

    Добавил код в обработчик кнопки, сохранил и протестировал приложение

--- WinBD/Form1.Designer.cs
+++ WinBD/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Click to Excute DataReader";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // listBox1
             // 
